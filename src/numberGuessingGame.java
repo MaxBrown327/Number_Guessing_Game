89ace2291410e21cd3363967748ec4a2f88c60de
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-21 14:05:19 -0400

    Added the meat of it all

--- src/numberGuessingGame.java
+++ src/numberGuessingGame.java
@@ -1,11 +1,49 @@
+import java.util.Scanner;
+
 public class numberGuessingGame {
 
-    public static void main{String[]args}{
+    public static void main(String[]args) {
 
-        int pickedNumber;
+        Scanner keyboard = new Scanner(System.in);
+        int systemNumber;
+        int playerGuess;
+        boolean wonGame;
+        int numberOfGuesses;
 
-        pickedNumber = (int)(Math.random()*100);
+        wonGame = false;
+        numberOfGuesses = 6;
+
+
+
+        systemNumber = (int)(Math.random()*100);
+
+        System.out.println("System has given you a number, between 1-100");
+        System.out.println(systemNumber);
+
+
+        while (!wonGame && numberOfGuesses>0) {
+            System.out.println("try and guess it.");
+            playerGuess = keyboard.nextInt();
+            if (playerGuess > systemNumber){
+                System.out.println("That's too high go lower ");
+            }
+            else if (playerGuess < systemNumber){
+                System.out.println("That's too low go higher");
+            }
+            else if (playerGuess == systemNumber){
+                System.out.println("Good job you chose the correct number!");
+                wonGame = true;
+            }
+
+            numberOfGuesses --;
+        }
+
+        System.out.println("Thanks for playing");
+        System.out.println("You had "+ numberOfGuesses + " guess/es left" );
+
+
+        }
+
 
 
     }
-}
