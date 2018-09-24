dc9d6d6143e50c919609624931a5391c1637f0e3
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-24 13:59:20 -0400

    Added the play again feature thusly finishing the project

--- src/numberGuessingGame.java
+++ src/numberGuessingGame.java
@@ -7,44 +7,63 @@ public class numberGuessingGame {
         Scanner keyboard = new Scanner(System.in);
         int systemNumber;
         int playerGuess;
+        int playerChoice;
         boolean wonGame;
         int numberOfGuesses;
+        boolean playAgain;
 
-        wonGame = false;
-        numberOfGuesses = 10;
-
-
-        systemNumber = (int) (Math.random() * 100);
-
-        System.out.println("System has given you a number, between 1-100");
+        playAgain = false;
 
 
+        while (playAgain==false) {
+            wonGame = false;
+            numberOfGuesses = 10;
 
-        while (!wonGame && numberOfGuesses > 0) {
-            System.out.println("try and guess it.");
-            playerGuess = keyboard.nextInt();
-            if (playerGuess > systemNumber) {
-                System.out.println("That's too high go lower ");
-            } else if (playerGuess < systemNumber) {
-                System.out.println("That's too low go higher");
-            } else if (playerGuess == systemNumber) {
-                System.out.println("Good job you chose the correct number!");
-                wonGame = true;
+
+            systemNumber = (int) (Math.random() * 100);
+
+            System.out.println("System has given you a number, between 1-100");
+            System.out.println(systemNumber);
+
+
+            while (!wonGame && numberOfGuesses > 0) {
+                System.out.println("try and guess it.");
+                playerGuess = keyboard.nextInt();
+                if (playerGuess > systemNumber) {
+                    System.out.println("That's too high go lower ");
+                } else if (playerGuess < systemNumber) {
+                    System.out.println("That's too low go higher");
+                } else if (playerGuess == systemNumber) {
+                    System.out.println("Good job you chose the correct number!");
+                    wonGame = true;
+                }
+
+                numberOfGuesses--;
             }
 
-            numberOfGuesses--;
-        }
+            if (wonGame == true) {
+                System.out.println("You won! Great job.");
+                System.out.println("You had " + numberOfGuesses + " guess/es left");
+            }
 
-        if (wonGame == true) {
-            System.out.println("You won! Great job.");
-            System.out.println("You had " + numberOfGuesses + " guess/es left");
-        }
+            if (wonGame == false) {
+                System.out.println("How could you not even guess a number?");
+                System.out.println("Your number was " + systemNumber);
+            }
+            System.out.println("Would you like to play again? 1=Yes 2=No");
+            playerChoice = keyboard.nextInt();
 
-        if (wonGame == false) {
-            System.out.println("How could you not even guess a number?");
-            System.out.println("Your number was "+ systemNumber);
-        }
 
+            if (playerChoice == 2) {
+                System.out.println("Thanks for playing!");
+                playAgain = true;
+
+            }
+            if (playerChoice == 1){
+                playAgain = false;
+            }
+
+        }
 
     }
 
