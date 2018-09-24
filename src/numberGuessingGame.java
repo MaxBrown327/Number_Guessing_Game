a6085a009faeb845cc535f2f4d5d8192bfedeb01
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-24 13:44:22 -0400

    This is either the final version or near it

--- src/numberGuessingGame.java
+++ src/numberGuessingGame.java
@@ -11,39 +11,43 @@ public class numberGuessingGame {
         int numberOfGuesses;
 
         wonGame = false;
-        numberOfGuesses = 6;
+        numberOfGuesses = 10;
 
 
-
-        systemNumber = (int)(Math.random()*100);
+        systemNumber = (int) (Math.random() * 100);
 
         System.out.println("System has given you a number, between 1-100");
-        System.out.println(systemNumber);
 
 
-        while (!wonGame && numberOfGuesses>0) {
+
+        while (!wonGame && numberOfGuesses > 0) {
             System.out.println("try and guess it.");
             playerGuess = keyboard.nextInt();
-            if (playerGuess > systemNumber){
+            if (playerGuess > systemNumber) {
                 System.out.println("That's too high go lower ");
-            }
-            else if (playerGuess < systemNumber){
+            } else if (playerGuess < systemNumber) {
                 System.out.println("That's too low go higher");
-            }
-            else if (playerGuess == systemNumber){
+            } else if (playerGuess == systemNumber) {
                 System.out.println("Good job you chose the correct number!");
                 wonGame = true;
             }
 
-            numberOfGuesses --;
+            numberOfGuesses--;
         }
 
-        System.out.println("Thanks for playing");
-        System.out.println("You had "+ numberOfGuesses + " guess/es left" );
-
-
+        if (wonGame == true) {
+            System.out.println("You won! Great job.");
+            System.out.println("You had " + numberOfGuesses + " guess/es left");
         }
 
+        if (wonGame == false) {
+            System.out.println("How could you not even guess a number?");
+            System.out.println("Your number was "+ systemNumber);
+        }
 
 
     }
+
+
+
+}
